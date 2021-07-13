51d4b0ce2f6e5dd4c2dde6e7b489ebe016e1ac62
Rolan Veron Cruz <rolanvc@yahoo.com>
2021-07-13 18:14:57 +0800

    working on implementing the NEclasses

--- NE.py
+++ NE.py
@@ -1,4 +1,33 @@
 # This implements Neuroevolution through the NE class.
 
-class NeuroEvolution:
-    pass
+from abc import ABC, abstractmethod
+
+class NeuroEvolution(ABC):
+    def run(self):
+        self.initialize()
+        while not self.terminate():
+            self.compute_fitness()
+            self.recombine()
+            self.mutate()
+
+    @abstractmethod
+    def initialize(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def terminate(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def compute_fitness(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def recombine(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def mutate(self):
+        raise NotImplementedError()
+
+
--- SANE.py
+++ SANE.py
@@ -1,3 +1,19 @@
+from NE import NeuroEvolution
 
 
-class SANE:
+class SANE(NeuroEvolution):
+    def initialize(self):
+        raise NotImplementedError()
+
+    def terminate(self):
+        raise NotImplementedError()
+
+    def compute_fitness(self):
+        raise NotImplementedError()
+
+    def recombine(self):
+        raise NotImplementedError()
+
+    def mutate(self):
+        raise NotImplementedError()
+
